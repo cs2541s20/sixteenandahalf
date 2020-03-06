0ae31923424178dbcb2a5bad47408db34607694a
Cassell Robinson <crobinson66@gwu.edu>
2020-03-06 15:41:13 -0500

    Adding home and connectvars

---
+++ connectvars.php
@@ -0,0 +1,9 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'sixteenandahalf'); //replace this
+  define('DB_PASSWORD', 'Sixteen.5'); //edit this
+  define('DB_NAME', 'sixteenandahalf'); //edit this
+  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+  
+?>
