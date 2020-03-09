315e8cbe5e355a2c7cb10237c8ca6c7bcbd1b5c8
samuelgassman7 <samuelgassman7@gwmail.gwu.edu>
2020-03-09 13:13:46 -0400

    added temp session vars to other accounts

--- Deliverables/otheraccounts.php
+++ Deliverables/otheraccounts.php
@@ -1,5 +1,11 @@
 <?php
 
+/*testing variables: TODO delete later
+ *
+ */
+$_SESSION['viewtype'] = 'admin';
+$_SESSION['viewas'] = '1234';
+$_SESSION['uid'] = '1234';
 
 require_once("navbar.php");
 
