0d723b2a2849ecfb1f270276933ee4959c5037be
Cassell Robinson <crobinson66@gwu.edu>
2020-03-11 12:06:53 -0400

    create account

--- Deliverables/createaccount.php
+++ Deliverables/createaccount.php
@@ -16,39 +16,35 @@ require_once("navbar.php");
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Aliens Abducted Me - Report an Abduction</title>
+  <title>Create an Account!</title>
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-  <h2>Aliens Abducted Me - Report an Abduction</h2>
+  <h2>Create Account</h2>
 
-  <p>Share your story of alien abduction:</p>
-  <form method="post" action="report.php">
+  <p>Create an Account:</p>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
     <label for="firstname">First name:</label>
     <input type="text" name="firstname" /><br />
     <label for="lastname">Last name:</label>
     <input type="text" name="lastname" /><br />
     <label for="email">What is your email address?</label>
     <input type="text" name="email" /><br />
-    <label for="whenithappened">When did it happen?</label>
-    <input type="text" name="whenithappened" /><br />
-    <label for="howlong">How long were you gone?</label>
-    <input type="text" name="howlong" /><br />
-    <label for="howmany">How many did you see?</label>
-    <input type="text" name="howmany" /><br />
-    <label for="aliendescription">Describe them:</label>
-    <input type="text" name="aliendescription" size="32" /><br />
-    <label for="whattheydid">What did they do to you?</label>
-    <input type="text" name="whattheydid" size="32" /><br />
+    
+    
+    <label for="type of user">What type of user?</label>
+    <input type="text" name="type of user" /><br />
+
+    <label for="userrID">Enter userID:</label>
+    <input type="text" name="userID" size="32" /><br />
+
+    <label for="password">Enter password?</label>
+    <input type="text" name="password" size="32" /><br />
+
     <label for="fangspotted">Have you seen my dog Fang?</label>
-    Yes <input name="fangspotted" type="radio" value="yes" />
-    No <input name="fangspotted" type="radio" value="no" /><br />
-    <img src="fang.jpg" width="100" height="175"
-      alt="My abducted dog Fang." /><br />
     <label for="other">Anything else you want to add?</label>
     <textarea name="other"></textarea><br />
-    <input type="submit" value="Report Abduction" name="submit" /><br/><br/>
-    <input type="submit" value="Who has seen Fang?" formaction="seen_fang.php"/>
+    <input type="submit" value="Create Account" name="Create Accont" /><br/><br/>
   </form>
 </body>
 </html>
