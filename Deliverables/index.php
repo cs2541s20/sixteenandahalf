b702f7922b052a7d2ed480ee0524b810f10b9b24
Cassell Robinson <crobinson66@gwu.edu>
2020-03-25 12:59:36 -0400

    added reset button

--- Deliverables/index.php
+++ Deliverables/index.php
@@ -4,10 +4,13 @@ session_start();
 if(!isset($_SESSION['viewtype'])){
 	header('Location: login.php');
 }
-
+require_once("connectvars.php");
 if(isset($_POST['reset'])){
 	echo "reseting";
-	
+	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	$commands = file_get_contents("databasesProject.sql");
+	$this->$dbc->multi_query($commands);
+}	
 
 
 }
@@ -19,5 +22,5 @@ require_once("navbar.php");
 
 <form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
 <input type = "submit", value = "RESET", name = "reset"/>
-
+</form>
 </body>
