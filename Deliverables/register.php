6161142786b833f05c3f1d269345e89dda248415
samuelgassman7 <samuelgassman7@gwmail.gwu.edu>
2020-03-25 10:08:47 -0400

    merged new navbar and register into master

--- Deliverables/register.php
+++ Deliverables/register.php
@@ -3,7 +3,7 @@ session_start();
 if(!isset($_SESSION['uid'])){
 	header('Location: login.php');
 }
-else if($_SESSION['viewtype'] != "student" && $_SESSION['viewtype'] != 'faculty'){
+else if($_SESSION['viewtype'] != "student" /*&& $_SESSION['viewtype'] != 'faculty'*/){
 	header('Location: index.php');
 }
 require_once('connectvars.php');
