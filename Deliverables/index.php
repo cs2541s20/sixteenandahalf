2668f7694826189481b49dc0d4450c1f3cda04f9
Cassell Robinson <crobinson66@gwu.edu>
2020-03-25 12:53:15 -0400

    made sql adjustments to table requirements

--- Deliverables/index.php
+++ Deliverables/index.php
@@ -4,10 +4,20 @@ session_start();
 if(!isset($_SESSION['viewtype'])){
 	header('Location: login.php');
 }
+
+if(isset($_POST['reset'])){
+	echo "reseting";
+	
+
+
+}
 require_once("navbar.php");
 ?>
 <html><H2>Welcome to Banweb 2.0 </H2></html>
 
 <body onload="navbar();">
+
+<form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
+<input type = "submit", value = "RESET", name = "reset"/>
 
 </body>
