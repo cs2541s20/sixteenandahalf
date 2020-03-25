7f7e1707aeba964a12ffd1b49fde1885d635dffe
Cassell Robinson <crobinson66@gwu.edu>
2020-03-25 01:42:56 -0400

    Finished register.php for testing

--- Deliverables/register.php
+++ Deliverables/register.php
@@ -16,12 +16,22 @@ $user_id = $_SESSION['uid'];
 require_once("navbar.php");
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
 
+if(isset($_POST['submit'])){
+    $user_crn = mysqli_real_escape_string($dbc, trim($_POST['CRN']));
+    $query = "SELECT * from course where crn ='$user_crn'";
+    $data = mysqli_query($dbc, $query);
+    if($row = mysqli_fetch_array($data) == true){
+      $sql = "INSERT INTO enrollemnt VALUES ('$user_id', '$user_crn', 'Fall', 'Sophomore', NULL, false)";
+      if($dbc->query($sql) === TRUE){
+        echo $name . " Course Added" ;
+      }
+      if(!$user_crn){
+        echo 'No Results';
+      }
+    }
+  }
 
 
-//$sql = "INSERT INTO enrollemnt VALUES ('$user_id', )";
-//if($dbc->query($sql) === TRUE){
- echo $name . " Course Added" ;
-//}
 
 ?>
 <html>
